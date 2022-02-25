c66d1333a0205fb842fd492437dcbd0f6dccc4ed
baby-thailand <swisscardiac@gmail.com>
2022-02-25 10:36:02 +0700

    up file drmax

--- assets/notify.js
+++ assets/notify.js
@@ -182,7 +182,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=600493857630184,932767647301597,672084573820817';
+            window.location.href = './thankyou.html?fb_pixel_id=1191384564936792';
         });
         
     }
